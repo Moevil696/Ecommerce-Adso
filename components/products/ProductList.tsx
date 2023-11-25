9ceb15a1126820da36b972126824e481a8d64dd0
Moevil696 <joker041999@gmail.com>
2023-11-25 14:03:29 -0500

    Add: component productList and ProductCard

---
+++ components/products/ProductList.tsx
@@ -0,0 +1,23 @@
+import { Props } from "next/script"
+import { IProduct } from "../../interfaces"
+import { ProductCard } from "./ProductCard"
+import { Grid } from "@mui/material"
+import { FC } from "react"
+
+interface Props {
+    products: IProduct[]
+}
+export const ProductList: FC<Props> = ({products}) => {
+    return (
+    <Grid container spacing={4}>
+        {
+            products.map((product) => (
+                <ProductCard
+                key={product}
+                    product={product}
+                />
+            ))
+        }
+    </Grid>
+    )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import { CardActionArea, Card, CardMedia, Grid, Typography } from '@mui/material
 import { ShopLayout } from '../components/layouts/ShopLayaout'
 
 import { initialData } from '../database/products'
+import { ProductList } from '../components/products';
 
 export default function Home() {
   return (
@@ -10,21 +11,7 @@ export default function Home() {
         <Typography variant='h1' component='h1'>Tienda</Typography>
         <Typography variant='h2' sx={{ mb: 1 }}>Articulos</Typography>
 
-        <Grid container spacing={4}>
-          {initialData.products.map((product) => (
-            <Grid item xs={6} sm={4} key={product.slug}>
-              <Card>
-                <CardActionArea>
-                  <CardMedia
-                    component='img'
-                    image={`products/${product.images[0]}`}
-                    alt={product.title}
-                  />
-                </CardActionArea>
-              </Card>
-            </Grid>
-          ))}
-        </Grid>
+      <ProductList products={initialData.products as any} />
       </ShopLayout>
     </>
   );
